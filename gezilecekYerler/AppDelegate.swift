e6baf501d513863df9c304381232248d2b1cb9ef
hasan çiftçi <>
2020-04-26 17:54:17 +0300

    firebase auth deneme

--- gezilecekYerler/AppDelegate.swift
+++ gezilecekYerler/AppDelegate.swift
@@ -18,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Auth.auth().signInAnonymously() { (authResult, error) in
+             guard let user = authResult?.user else { return }
+             let isAnonymous = user.isAnonymous  // true
+             let uid = user.uid
+               print(uid)
+           }
         return true
     }
 
